fad226287843f53f754fe5cfacbb3e34aa71c327
unknown <Shobhan>
2025-12-23 15:07:24 +0530

    updated rejectUnauthorized

--- db/sequelize.js
+++ db/sequelize.js
@@ -15,7 +15,7 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: true, // IMPORTANT
+      rejectUnauthorized: false, // IMPORTANT
       ca: ca,
     },
   },
