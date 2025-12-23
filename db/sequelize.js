840d3b7edeeb46f4c92fb39e0a350a1993d17581
unknown <Shobhan>
2025-12-23 15:45:15 +0530

    removed certificates

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,13 +1,4 @@
 import { Sequelize } from "sequelize";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const caPath = path.join(__dirname, "../certs/prod-ca-2021.crt");
-const ca = fs.readFileSync(caPath);
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
@@ -16,7 +7,6 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
     ssl: {
       require: true,
       rejectUnauthorized: false, // IMPORTANT
-      //ca: ca,
     },
   },
 });
