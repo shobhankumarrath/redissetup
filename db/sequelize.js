6e3d09422f8864029abc38d577a2ed7b38881bb6
unknown <Shobhan>
2025-12-23 16:22:00 +0530

    updated seq.js

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,13 +1,5 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
   logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // IMPORTANT
-    },
-  },
-  ssl: true,
 });
