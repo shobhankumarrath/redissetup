94ca15212f078cc9f62b465f8670987665880faf
unknown <Shobhan>
2025-12-29 01:14:47 +0530

    error resolving

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,5 +1,10 @@
 import { Sequelize } from "sequelize";
-
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  logging: false,
-});
+let sequelize;
+try {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    logging: false,
+  });
+} catch (error) {
+  console.error(error);
+}
+export { sequelize };
